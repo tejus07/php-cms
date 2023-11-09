b4413bb85142402b5a8c6f24a1971dfdde5d0ea2
Tejus Sahi <tejussahi07@gmail.com>
2023-11-09 23:32:00 +0530

    Added admin v1

---
+++ admin/admin-sidebar.php
@@ -0,0 +1,30 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="brands.php">
+              <span data-feather="file"></span>
+              Brands
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Phones
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Users
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
