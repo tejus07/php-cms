d411a79aa946fef16b5546709608b99f8afdf59e
Kamran Khan <kamrankhn82@gmail.com>
2023-11-15 21:43:24 +0530

    Support for sorting added

--- index.php
+++ index.php
@@ -1,18 +1,63 @@
 <?php
-include_once 'shared/database.php';
+try {
+	include_once 'shared/database.php';
 
-$database = new Database();
+	$database = new Database();
 
-$pdo = $database->getConnection();
+	$pdo = $database->getConnection();
 
-$recipesList = $pdo->query('SELECT * FROM recipes');
+	$isUserLoggedIn = isset($_SESSION['user_id']);
+
+	$query = 'SELECT * FROM recipes';
+
+	if ($isUserLoggedIn) {
+		$sortOrder = isset($_GET['sort']) ? $_GET['sort'] : 'title__DESC';
+
+		$sort = explode('__', $sortOrder);
+
+		$sortColumn = isset($sort[0]) ? $sort[0] : 'created_at';
+		$sortDirection = isset($sort[1]) ? $sort[1] : 'ASC';
+
+		if (!in_array($sortColumn, ['created_at', 'title', "preparation_time"]) || !in_array($sortDirection, ['ASC', 'DESC'])) {
+    		throw new Exception("Invalid sorting parameters.");
+    	}
+		$query = "SELECT * 
+		FROM recipes
+		ORDER BY $sortColumn " . ($sortDirection === 'ASC' ? 'ASC' : 'DESC');
+	}
+
+    $stmt = $pdo->prepare($query);
+
+    $stmt->execute();
+
+    $recipesList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 ?>
     <?php include('shared/header.php');?>
 
     <main>
+		<?php
+			if ($isUserLoggedIn) { 
+		?>
+		<form method="GET" >
+			<label for="sort">Sort by:</label>
+    		<select name="sort" id="sort">
+        		<option value="title__ASC" <?php echo ($sortOrder === 'title__ASC') ? 'selected' : ''; ?>>Title: A to Z</option>
+        		<option value="title__DESC" <?php echo ($sortOrder === 'title__DESC') ? 'selected' : ''; ?>>Title: Z to A</option>
+        		<option value="created_at__ASC" <?php echo ($sortOrder === 'created_at__ASC') ? 'selected' : ''; ?>>Date: New to Old</option>
+        		<option value="created_at__DESC" <?php echo ($sortOrder === 'created_at__DESC') ? 'selected' : ''; ?>>Date: Old to New</option>
+        		<option value="preparation_time__ASC" <?php echo ($sortOrder === 'preparation_time__ASC') ? 'selected' : ''; ?>>Preparation Time: Quickest to Longest</option>
+        		<option value="preparation_time__DESC" <?php echo ($sortOrder === 'preparation_time__DESC') ? 'selected' : ''; ?>>Preparation Time: Longest to Quickest</option>
+    		</select>
+    		<button type="submit">Sort</button>
+		</form>
+		<?php }?>
         <?php
-        while ($post = $recipesList->fetch(PDO::FETCH_ASSOC)) {
+        foreach ($recipesList as $post) {
 			$date = date_create($post['created_at']);					
 			$message = str_replace("\n\r", "<br><br>", $post['description']);
 			?>
