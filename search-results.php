d0693700194c40268ff291dc0d8f15fc8b4182f6
Tejus Sahi <tejussahi07@gmail.com>
2023-11-21 13:49:06 +0530

    Working on search results

---
+++ search-results.php
@@ -0,0 +1,112 @@
+<?php
+$title = 'Search';
+require_once 'includes/header.php';
+require 'db.php';
+require 'navbar.php';
+try {
+    $search = isset($_GET['search']) ? $_GET['search'] : ''; // Get the search query
+
+    // if (!$search):
+    // header("Location: index.php");
+    // endif;
+
+    $sortOrder = isset($_GET['sort']) ? $_GET['sort'] : 'name-ASC'; // Get the selected sorting option or default to 'name_ASC'
+    $sort = explode('-', $sortOrder);
+    $sortColumn = isset($sort[0]) ? $sort[0] : 'name'; // Get sort column or default to 'name'
+    $sortDirection = isset($sort[1]) ? $sort[1] : 'ASC'; // Get sort direction or default to 'ASC'
+
+    $stmt = $pdo->prepare("SELECT phones.*, brands.name AS brand_name 
+                            FROM phones 
+                            INNER JOIN brands ON phones.brand_id = brands.id 
+                            ORDER BY $sortColumn " . ($sortDirection === 'ASC' ? 'ASC' : 'DESC')); // Order by the selected column and direction
+    // $stmt->bindValue(':brandId', $brand_id);
+    $stmt->execute();
+
+    $phones = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <form method="GET">
+                <label for="sort">Sort by:</label>
+                <select name="sort" id="sort">
+                    <?php
+                    $selectedSort = isset($_GET['sort']) ? $_GET['sort'] : '';
+
+                    $options = [
+                        'name-ASC' => 'Name (Ascending)',
+                        'name-DESC' => 'Name (Descending)',
+                        'created_at-ASC' => 'Created At (Ascending)',
+                        'created_at-DESC' => 'Created At (Descending)',
+                        'release_date-ASC' => 'Released At (Ascending)',
+                        'release_date-DESC' => 'Released At (Descending)'
+                    ];
+
+                    foreach ($options as $value => $label) {
+                        $selected = ($value === $selectedSort) ? 'selected' : '';
+                        echo "<option value=\"$value\" $selected>$label</option>";
+                    }
+                    ?>
+                </select>
+                <button type="submit">Sort</button>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <form method="GET" action="search.php">
+                <label for="phoneFilter">Filter by Phone:</label>
+                <select name="phoneFilter" id="phoneFilter">
+                    <!-- Populate options dynamically based on phone attributes or criteria -->
+                    <option value="phone_criteria_1">Phone Criteria 1</option>
+                    <option value="phone_criteria_2">Phone Criteria 2</option>
+                    <!-- Add more options as needed -->
+                </select>
+                <br>
+                <label for="brandFilter">Filter by Brand:</label>
+                <select name="brandFilter" id="brandFilter">
+                    <!-- Populate options dynamically based on available brands -->
+                    <option value="brand_criteria_1">Brand Criteria 1</option>
+                    <option value="brand_criteria_2">Brand Criteria 2</option>
+                    <!-- Add more options as needed -->
+                </select>
+                <br>
+                <button type="submit">Search</button>
+            </form>
+
+        </div>
+        <div class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <?php
+            foreach ($phones as $phone) {
+                echo '<div class="card">';
+                if ($phone['image_url']) {
+                    echo '<img src="./uploads/' . $phone['image_url'] . '" class="bd-placeholder-img card-img-top" width="100%" height="180" alt="' . $phone['name'] . '">';
+                } else {
+                    echo '<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img"
+    aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false">
+    <title>Placeholder</title>
+    <rect width="100%" height="100%" fill="#6c757d"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">' . $phone['name'] . '</text>
+</svg>';
+                }
+                echo '<div class="card-body">';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">' . $phone['name'] . '</h5>';
+                echo '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s
+    content.</p>';
+                echo '<p>Release Date: ' . $phone['release_date'] . '</p>';
+                echo '<p>Brand: ' . $phone['brand_name'] . '</p>';
+                echo '<a href="view-phone.php?id=' . $phone['id'] . '" class="btn btn-primary">View</a>';
+                echo '</div>';
+                echo '</div></div>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+require_once 'includes/footer.php';
+?>
