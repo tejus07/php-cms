1be56d5482512e215a2cd451b45dc0cbaf92b853
Tejus Sahi <tejussahi07@gmail.com>
2023-11-23 23:19:17 +0530

    A typo mistake in view recipe

--- view_post.php
+++ view_post.php
@@ -41,7 +41,7 @@ $stmt->bindParam(':recipeId', $recipe_id, PDO::PARAM_INT);
 $stmt->execute();
 
 
-$data = $stmt->fetch(PDO::FETCH_ASSOC);
+$recipe = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // $stmt = $pdo->prepare($sqlQuery);
     // $stmt->execute([$query]);
@@ -53,32 +53,32 @@ $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
 <main>
 <div class="container">
-        <h1 class="recipe-title"><?php echo $data['recipe_title']; ?></h1>
-		<img src="<?php echo $data['image_url'] ?>" width="500">
-        <p class="recipe-description"><?php echo $data['recipe_description']; ?></p>
+        <h1 class="recipe-title"><?php echo $recipe['recipe_title']; ?></h1>
+		<img src="<?php echo $recipe['image_url'] ?>" width="500">
+        <p class="recipe-description"><?php echo $recipe['recipe_description']; ?></p>
 
         <h2 class="section-title">Recipe Details</h2>
         <ul class="details-list">
-            <li class="detail-item">Preparation Time: <?php echo $data['preparation_time']; ?> minutes</li>
-            <li class="detail-item">Cooking Time: <?php echo $data['cooking_time']; ?> minutes</li>
-            <li class="detail-item">Servings: <?php echo $data['servings']; ?></li>
-            <li class="detail-item">Difficulty Level: <?php echo $data['difficulty_level']; ?></li>
-            <li class="detail-item">Cuisine: <?php echo $data['cuisine']; ?></li>
-            <li class="detail-item">Course: <?php echo $data['course']; ?></li>
+            <li class="detail-item">Preparation Time: <?php echo $recipe['preparation_time']; ?> minutes</li>
+            <li class="detail-item">Cooking Time: <?php echo $recipe['cooking_time']; ?> minutes</li>
+            <li class="detail-item">Servings: <?php echo $recipe['servings']; ?></li>
+            <li class="detail-item">Difficulty Level: <?php echo $recipe['difficulty_level']; ?></li>
+            <li class="detail-item">Cuisine: <?php echo $recipe['cuisine']; ?></li>
+            <li class="detail-item">Course: <?php echo $recipe['course']; ?></li>
         </ul>
 
         <h2 class="section-title">Instructions</h2>
-        <p class="instructions"><?php echo $data['instructions']; ?></p>
+        <p class="instructions"><?php echo $recipe['instructions']; ?></p>
 
         <h2 class="section-title">Ingredients</h2>
-        <p class="ingredients"><?php echo $data['ingredients']; ?></p>
+        <p class="ingredients"><?php echo $recipe['ingredients']; ?></p>
 
         <h2 class="section-title">Category</h2>
-        <p class="category"><?php echo $data['category_name']; ?></p>
+        <p class="category"><?php echo $recipe['category_name']; ?></p>
 
         <h2 class="section-title">User Information</h2>
-        <p class="user-info">Username: <?php echo $data['user_username']; ?></p>
-        <p class="user-info">Email: <?php echo $data['user_email']; ?></p>
+        <p class="user-info">Username: <?php echo $recipe['user_username']; ?></p>
+        <p class="user-info">Email: <?php echo $recipe['user_email']; ?></p>
 
         <a class="btn" href="index.php">Back to Recipes</a>
     </div>
