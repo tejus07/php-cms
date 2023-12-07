dcb1842e605d2092d7ced5b4a57bbceaa50e2b68
00anandmishra <00anandmishra@gmail.com>
2023-12-08 00:20:56 +0530

    image resizing feature added

--- admin/shared/image_handler.php
+++ admin/shared/image_handler.php
@@ -21,10 +21,60 @@ class ImageHandler {
 
         return $extension_valid && $mime_type_valid;
     }
+    public function resize_image($source_path, $target_path, $max_width, $max_height) {
+
+        list($original_width, $original_height, $type) = getimagesize($source_path);
+
+        $ratio = $original_width / $original_height;
+        $new_width = $max_width;
+        $new_height = $max_height;
+
+        if ($new_width / $new_height > $ratio) {
+            $new_width = $new_height * $ratio;
+        } else {
+            $new_height = $new_width / $ratio;
+        }
+
+        $new_image = imagecreatetruecolor($new_width, $new_height);
+
+        $source_image = $this->create_image_from_type($type, $source_path);
+
+        imagecopyresampled($new_image, $source_image, 0, 0, 0, 0, $new_width, $new_height, $original_width, $original_height);
+        $this->save_image_by_type($type, $new_image, $target_path);
+
+        imagedestroy($new_image);
+        imagedestroy($source_image);
+    }
+
+    private function create_image_from_type($type, $source_path) {
+        switch ($type) {
+            case IMAGETYPE_JPEG:
+                return imagecreatefromjpeg($source_path);
+            case IMAGETYPE_PNG:
+                return imagecreatefrompng($source_path);
+            case IMAGETYPE_GIF:
+                return imagecreatefromgif($source_path);
+            default:
+                throw new Exception('Unsupported image format');
+        }
+    }
+
+    private function save_image_by_type($type, $image, $target_path) {
+        switch ($type) {
+            case IMAGETYPE_JPEG:
+                imagejpeg($image, $target_path, 90);
+                break;
+            case IMAGETYPE_PNG:
+                imagepng($image, $target_path, 9);
+                break;
+            case IMAGETYPE_GIF:
+                imagegif($image, $target_path);
+                break;
+        }
+    }
 
 
-
-    public function upload_image($imageFile) {
+    public function upload_image($imageFile, $resize = true, $max_width = 400, $max_height = 250) {
         if ($imageFile['error'] !== UPLOAD_ERR_OK) {
             throw new Exception("File upload error: " . $imageFile['error']);
         }
@@ -38,6 +88,10 @@ class ImageHandler {
         $target_file = $uploads_dir . $image_filename;
     
         if (move_uploaded_file($imageFile['tmp_name'], "../".$target_file)) {
+            if ($resize) {
+                echo "in here";
+                $this->resize_image("../".$target_file, "../".$target_file, $max_width, $max_height);
+            }
             return $target_file;
         } else {
             throw new Exception("Failed to move the uploaded file.");
